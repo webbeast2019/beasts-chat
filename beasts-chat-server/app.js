e1210546f121faa8f4a4fe61516f4e6e5639f15a
yuvalbl <yuval.bar.levi@gmail.com>
2019-08-21 20:42:16 +0300

    basic chat

--- beasts-chat-server/app.js
+++ beasts-chat-server/app.js
@@ -1,12 +1,18 @@
+const http = require('http');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const chat = require('./chat.service');
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+console.log(chat);
+chat(io); // connect chat service
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
-const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,4 +23,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+module.exports = {app, server};
---
+++ beasts-chat-server/chat.service.js
@@ -0,0 +1,17 @@
+module.exports = (io) => {
+  io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
+    });
+  });
+
+  io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+    });
+  });
+};
