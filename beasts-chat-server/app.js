583738e6a9716ee5f0525230a308d017a4b823f4
klogpok <kloleg@gmail.com>
2019-08-21 20:24:26 +0300

    Init commit

--- beasts-chat-server/app.js
+++ beasts-chat-server/app.js
@@ -3,10 +3,14 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const PORT = process.env.PORT || 5000; 
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
-const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +20,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
+});
+
+http.listen(PORT, function(){
+    console.log(`listening on port :${PORT}`);
+  });
 
 module.exports = app;
