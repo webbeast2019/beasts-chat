e9f2894f4ff7a9e2bd4fb1a35f354c209e0b4144
Ben <benedry@gmail.com>
2019-08-30 10:10:45 +0300

    editing server

--- beasts-chat-server/routes/index.js
+++ beasts-chat-server/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 // /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
@@ -9,6 +8,5 @@ const router = express.Router();
 router.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
 
 module.exports = router;
