c33b0fc85ce4acce6c4792b42354675de80311a2
Ben <benedry@gmail.com>
2019-09-04 17:31:01 +0300

    delete keyss

--- beasts-chat-server/config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    google: {
-        clientID: '1021899124905-prslr1sac745pq7rkhnquuvfbsfs92td.apps.googleusercontent.com',
-        clientSecret: 'A0YD-tNEsAlbMDtxpwbgSAjT',
-        mongoAtlas: {
-            dbURI: "mongodb+srv://test:test1234@cluster0-a1bzx.mongodb.net/test?retryWrites=true&w=majority",
-        }
-    }
-};
